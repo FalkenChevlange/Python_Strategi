66a8c5969d54f5fe033a0d00df0194284ed6491c
Anders <anders.sjoholm@gmail.com>
2024-07-27 00:48:16 +0200

    Normalize all the line endings

--- OLD/Fungerande 2024-07-26 strukturerad/backtest/calculate_metrics.py
+++
@@ -1,4 +0,0 @@
-def calculate_performance_metrics(portfolio_returns):
-    metrics = portfolio_returns.groupby('portfolio', observed=True)['holding_return'].agg(['mean', 'std']).reset_index()
-    metrics['sharpe'] = metrics['mean'] / metrics['std']
-    return metrics
--- OLD/Fungerande version 2024-07-04_databas_pris_rapport för flera bolag/borsdata_api/constants.py
+++
@@ -1,7 +0,0 @@
-import os
-
-# Define the export path
-EXPORT_PATH = r'C:\Users\ander\Documents\Borsdata\Python_Strategi\Exportdata'
-DB_FILE = os.path.join(EXPORT_PATH, 'borsdata.db')
-
-API_KEY = "9c5536f969dc44ce980aee4acbf798b3"  # Replace with your actual API key
