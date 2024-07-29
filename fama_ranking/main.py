c99819eed80545e8f7ced5c1fd363e50fea33a64
Anders <anders.sjoholm@gmail.com>
2024-07-29 15:28:48 +0200

    Spara buggfri version

--- fama_ranking/main.py
+++ fama_ranking/main.py
@@ -8,17 +8,26 @@ sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from helpers.db_utils import connect_db
 from calculate_factors import calculate_rolling_factors, calculate_factors
 from rank_factors import rank_factors
-from save_rankings import save_rankings_to_db
+from save_rankings import save_rankings
 
 def main():
     conn = connect_db()
+    print("Reading monthly_price_data...")
     price_df = pd.read_sql_query("SELECT * FROM monthly_price_data", conn)
+    print("Reading monthly_report_data...")
     report_df = pd.read_sql_query("SELECT * FROM monthly_report_data", conn)
+    
+    print("Calculating rolling factors...")
     rolling_report_df = calculate_rolling_factors(report_df)
+    print("Calculating factors...")
     factors_df = calculate_factors(price_df, rolling_report_df)
+    print("Ranking factors...")
     ranked_df = rank_factors(factors_df)
-    save_rankings_to_db(ranked_df)
+    
+    print("Saving rankings to database...")
+    save_rankings(ranked_df)
     conn.close()
+    print("Done.")
 
 if __name__ == "__main__":
     main()
--- fama_ranking/save_rankings.py
+++ fama_ranking/save_rankings.py
@@ -1,11 +1,15 @@
 import sqlite3
+from helpers.db_utils import connect_db
 
 def save_rankings_to_db(df):
-    conn = sqlite3.connect('path_to_your_database.db')
-    df.to_sql('rankings', conn, if_exists='replace', index=False)
+    conn = connect_db()
+    print("Saving to SQL...")
+    df.to_sql('factor_rankings', conn, if_exists='replace', index=False)
+    print("Data saved to SQL.")
     conn.close()
 
 def save_rankings(df):
     # Justera för att inkludera 'value_factor_rank' istället för 'value_rank'
     ranking_df = df[['ins_id', 'date', 'size_rank', 'value_factor_rank', 'profitability_rank', 'momentum_rank', 'volatility_rank']]
+    print("Saving adjusted rankings...")
     save_rankings_to_db(ranking_df)
